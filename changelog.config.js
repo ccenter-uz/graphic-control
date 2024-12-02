1149d04114f8f72413a83c562fafa1b772b6f722
abdulqodir18 <abduqodirdev@gmail.com>
2024-12-02 16:56:10 +0500

    refactor(changelog): Refactor CHANGELOG config

---
+++ changelog.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  types: [
+    { type: "feat", section: "Features" },
+    { type: "fix", section: "Bug Fixes" },
+    { type: "chore", section: "Chores" },
+    { type: "docs", section: "Documentation" },
+    { type: "style", section: "Code Style Changes" },
+    { type: "refactor", section: "Refactors" },
+    { type: "test", section: "Tests" },
+    { type: "perf", section: "Performance Improvements" },
+    { type: "ci", section: "CI/CD" },
+  ],
+};
