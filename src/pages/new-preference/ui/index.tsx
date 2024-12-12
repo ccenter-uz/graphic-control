e3f40ebb5978b3090029e1b68770acab003d5522
Umirzakovv <muhammadyosinumirzakov601@gmail.com>
2024-12-12 17:00:42 +0500

    feat(core): Create preference the same like last month preference

--- src/pages/new-preference/ui/index.tsx
+++ src/pages/new-preference/ui/index.tsx
@@ -1,19 +1,115 @@
+import { useState } from "react";
 import { useTranslation } from "react-i18next";
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
+import { API_MAP } from "@shared/constants/apiMap";
 import { scheduleLinks } from "@shared/constants/local-data";
 import { clockPath } from "@shared/constants/svg-paths";
+import { baseApi } from "@shared/lib/baseApi";
+import { ICheckbox } from "@shared/lib/types";
+import { HttpStatusCode } from "@shared/model/httpStatus";
 import BackLink from "@shared/ui/back-link";
 import BaseContainer from "@shared/ui/base-cotainer";
 import BaseLink from "@shared/ui/base-link";
+import ConfirmModal from "@shared/ui/confirm-modal";
 import HeaderContainer from "@shared/ui/header-container";
 import HeaderTitle from "@shared/ui/header-title";
+import { Loader } from "@shared/ui/loader";
 import UserProfileLink from "@shared/ui/user-profile-link";
+
+interface IPreference {
+  id: string;
+  offDays: string[];
+  workingHours: string;
+  daysOfMonth: ICheckbox[];
+  description: string;
+  requested_date: string;
+}
 
 export const NewPreference = () => {
   const { t } = useTranslation();
   const [timeParams] = useSearchParams();
   const storedWorkingHours = localStorage.getItem("workingHours") as string;
+  const token = localStorage.getItem("GCToken") as string;
+  const navigate = useNavigate();
+
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [isBtnLoading, setIsLoading] = useState<boolean>(false);
+  const [isConfirmLoading, setConfirmIsLoading] = useState<boolean>(false);
+  const [errorText, setError] = useState<string>("");
+  const [preference, setPreference] = useState<IPreference>();
+
+  const today = new Date();
+  const month = today.getMonth();
+  const year = today.getFullYear();
+
+  const handleBtnClick = async () => {
+    try {
+      setIsLoading(true);
+      const matchYear = month === 1 ? year - 1 : year;
+      const matchMonth = month === 1 ? 12 : month + 1;
+
+      const res = await baseApi.get(
+        `${API_MAP.GET_PREFERENCES_BY_YEAR}${matchYear}&month=${matchMonth}`,
+        {
+          headers: {
+            accept: "*/*",
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      );
+      if (res.status === HttpStatusCode.OK) {
+        const neededId = res.data.result[0].id;
+
+        try {
+          const res = await baseApi.get(
+            `${API_MAP.GET_SINGLE_PREFERENCE_BY_ID}${neededId}`,
+            {
+              headers: {
+                accept: "*/*",
+                Authorization: `Bearer ${token}`,
+              },
+            },
+          );
+          if (res.status === HttpStatusCode.OK) {
+            const neededYear = month === 11 ? year + 1 : year;
+            const neededMonth = month === 11 ? 1 : month + 1;
+            const data = res.data;
+            setPreference({
+              ...data,
+              requested_date: `${neededYear}/${neededMonth}`,
+            });
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    } catch (error: unknown) {
+      setError((error as Error).message);
+    } finally {
+      setIsLoading(false);
+      setIsModalOpen(!isModalOpen);
+    }
+  };
+
+  const modalConfirmClick = async () => {
+    try {
+      setConfirmIsLoading(true);
+      const res = await baseApi.post(API_MAP.CREATE_PREFERENCE, preference, {
+        headers: {
+          accept: "*/*",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (res.status === HttpStatusCode.CREATED) {
+        navigate("/done");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setConfirmIsLoading(false);
+    }
+  };
 
   return (
     <BaseContainer className="bg-[#F9FDFF]">
@@ -23,11 +119,32 @@ export const NewPreference = () => {
         <UserProfileLink />
       </HeaderContainer>
       <div className="px-6 mt-6">
-        <BaseLink
-          title={t("new-preference.main-btn-title")}
-          to="/"
-          isBlue={true}
-        />
+        <button
+          onClick={handleBtnClick}
+          className="w-full border rounded px-4 py-3 text-[#506DD7] text-sm min-h-[54px]"
+        >
+          {isBtnLoading ? (
+            <Loader />
+          ) : errorText ? (
+            errorText
+          ) : (
+            "Такой же как и в прошлом месяце"
+          )}
+        </button>
+        {isModalOpen ? (
+          <ConfirmModal
+            state={isModalOpen}
+            setState={setIsModalOpen}
+            confirmBtnClick={modalConfirmClick}
+            modalText={
+              "Вы действительно хотите использовать данные из прошлого месяца?"
+            }
+            confirmBtnTitle="Да"
+            loading={isConfirmLoading}
+          />
+        ) : (
+          ""
+        )}
         <div className="grid grid-rows-4 grid-flow-col gap-4 mt-6">
           {scheduleLinks?.map((item, index) => {
             timeParams.set("time", item?.time);
--- src/shared/ui/confirm-modal.tsx
+++ src/shared/ui/confirm-modal.tsx
@@ -1,14 +1,25 @@
 import { FC } from "react";
 
 import BaseButton from "./base-button";
+import { Loader } from "./loader";
 import SecondaryButton from "./secondary-button";
 
 type Props = {
   state: boolean;
   setState: React.Dispatch<React.SetStateAction<boolean>>;
-  confirmClick: () => void;
+  confirmBtnClick: () => void;
+  modalText?: string;
+  confirmBtnTitle?: string;
+  loading?: boolean;
 };
-const ConfirmModal: FC<Props> = ({ state, setState, confirmClick }) => {
+const ConfirmModal: FC<Props> = ({
+  state,
+  setState,
+  confirmBtnClick,
+  modalText,
+  confirmBtnTitle,
+  loading,
+}) => {
   const handleCancelClick = () => {
     setState(!state);
   };
@@ -22,16 +33,22 @@ const ConfirmModal: FC<Props> = ({ state, setState, confirmClick }) => {
         onClick={(e: React.MouseEvent<HTMLDivElement, MouseEvent>) =>
           e.stopPropagation()
         }
-        className="my-auto px-5 py-6 border rounded-lg bg-white z-[9999]"
+        className="min-w-[300px] my-auto px-5 py-6 border rounded-lg bg-white z-[9999]"
       >
-        <p className="text-center mb-8">Вы действительно хотите выйти?</p>
+        <p className="text-center mb-8">
+          {modalText ? modalText : "Вы действительно хотите выйти?"}
+        </p>
         <div className="flex justify-between items-center">
           <SecondaryButton className="w-1/3" onClick={handleCancelClick}>
             Отмена
           </SecondaryButton>
-          <BaseButton widthNotFull={true} onClick={confirmClick}>
-            Выйти
-          </BaseButton>
+          {loading ? (
+            <Loader />
+          ) : (
+            <BaseButton widthNotFull={true} onClick={confirmBtnClick}>
+              {confirmBtnTitle ? confirmBtnTitle : "Выйти"}
+            </BaseButton>
+          )}
         </div>
       </div>
     </div>
